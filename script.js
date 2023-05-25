1a3737d5cb690aa70f4a6cb795b0ba821bd7d887
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-05-25 18:46:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const text = document.getElementById("text");
-const delay = document.getElementById("delay");
-const btn = document.getElementById("btn");
-const output = document.getElementById("output");
+// const text = document.getElementById("text");
+// const delay = document.getElementById("delay");
+// const btn = document.getElementById("btn");
+// const output = document.getElementById("output");
 
-//your code here
+// //your code here
+// async function showMessage() {
+//   const message = text.value;
+//   const delayVal = delay.value;
+//   await new Promise((resolve) => setTimeout(resolve, delayVal));
+//   output.innerText = message;
+// }
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+
 async function showMessage() {
-  const message = text.value;
-  const delayVal = delay.value;
-  await new Promise((resolve) => setTimeout(resolve, delayVal));
+  const message = textInput.value;
+  const delay = delayInput.value;
+
+  await new Promise((resolve) => setTimeout(resolve, delay));
+
   output.innerText = message;
 }
+
+btn.addEventListener('click', showMessage);
 
 btn.addEventListener("click", showMessage);
