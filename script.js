1d25c301c5265bd3b82b54e7ac5390bee302af1f
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-02 08:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //your code here
 async function showMessage() {
-	const textInput = document.getElementById(`text`).value;
-    const delayInput = document.getElementById(`delay`).value;
+	const textInput = document.getElementById('text').value;
+    const delayInput = document.getElementById('delay').value;
 
 	if (textInput && delayInput) {
 		const delayInSec = parseInt(delayInput);
-		const outputDiv = document.getElementById(`output`);
+		const outputDiv = document.getElementById('output');
 
-		outputDiv.textContent = 'Waiting...';
+		outputDiv.textContent = '';
 		await new Promise((resolve) => setTimeout(resolve, delayInput*1000));
 
 		outputDiv.textContent = textInput;
 	}
 }
-const btn = document.getElementById("btn");
-btn.addEventListener("click", showMessage);
+const btn = document.getElementById('btn');
+btn.addEventListener('click', showMessage);
